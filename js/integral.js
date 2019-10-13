26ed95db58a26017938273ec94a8c7dfe068be1f
河愁 <l12096966@163.com>
2019-10-13 20:14:45 +0800

    开发 ： 积分兑换

--- js/integral.js
+++ js/integral.js
@@ -1,4 +1,26 @@
+var code_data = [],value = 1001;
 ;(function($){
+    $.post({
+        url: 'https://mall-app.sogrumpy.cn/app/api/v1/marketing/JiFenDuoBao/JiFenChouDetail',
+        headers: {
+         'X-Token': "e6c46781833345f4a94ccf51916ab61c000186b705da1dbc2",
+         'Content-Type': 'application/json',
+         },
+         data: JSON.stringify({ 'txnId': value}),
+        success: function(res){
+           
+           // 获取幸运码
+           code_data = res.data.luckyCodeList;
+           console.log(code_data)
+           var codeBox = $("#code_data").html();
+           var codeHtml = ejs.render(codeBox,code_data);
+           $('#codeBox').html(codeHtml);
+           
+        },
+        error: function() {
+         console.log(1);
+        }
+       });
     // 兑换次数
     var exc_num =Number($('#exc_num').text());
     $('#exc_cut').click(function(){
@@ -14,11 +36,34 @@
     // 弹窗交互
     var box1 = $('#box1'),box2=$('#box2');
     $('#exc_req').click(function(){
-        box1.addClass('block')
+        box1.addClass('block');
+        $('#num_count').text($('#exc_num').text()*100);
     });
+    // $('#button1_1').click(function(){
+    //     box1.removeClass('block');
+    //     box2.addClass('block')
+    // })
     $('#button1_1').click(function(){
         box1.removeClass('block');
-        box2.addClass('block')
+        $.post({
+            url: 'https://mall-app.sogrumpy.cn/app/api/v1/marketing/JiFenDuoBao/JiFenChouDetail',
+            headers: {
+             'X-Token': "e6c46781833345f4a94ccf51916ab61c000186b705da1dbc2",
+             'Content-Type': 'application/json',
+             },
+             data: JSON.stringify({ 'txnId': value}),
+            success: function(res){
+               // 获取幸运码
+               code_data = res.data.luckyCodeList;
+               console.log(code_data)
+               var codeBox = $("#code_data").html();
+               var codeHtml = ejs.render(codeBox,code_data);
+               $('#codeBox').html(codeHtml);
+            },
+            error: function() {
+             console.log(1);
+            }
+           });
     })
     $('#button1_2').click(function(){
         box1.removeClass('block')
